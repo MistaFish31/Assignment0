0b4b1ac14b1a069959cf8349556673a9fa9bad33
MistaFish31 <zhang.ma@husky.neu.edu>
2016-01-19 02:12:21 -0500

    init commit
    basic wolf drawing

--- sketch.js
+++ sketch.js
@@ -1,7 +1,50 @@
 function setup() {
-  
+  createCanvas(700,700);
 }
 
 function draw() {
+
+//legs
+  strokeWeight(6)
+  noFill()
+  arc(255,520,45,180,-.6,1.6)
+  arc(330,515,35,180,1.5,3.8)
+
+//tail
+  strokeWeight(0)
+  fill(170,120,120)
+  arc(360,420,95,110,-.6,2.2,CHORD)
+  fill(255,255,255)
+  arc(340,395,125,125,-.6,2.2,CHORD)
   
+//body
+  fill(150,100,100)
+  triangle(280,320,240,520,350,500)
+  
+//head
+  ellipse(280,320,140,130)
+  fill(170,120,120)
+  ellipse(240,300,68,65)
+  
+//nose
+  fill(0,0,0)
+  triangle(215,278,240,268,228,288)
+  
+//mouth
+  noFill()
+  strokeWeight(2)
+  arc(253,285,50,50,1.2,3)
+  arc(203,288,50,50,0,1.2)
+
+//eyes
+  fill(0,0,0)
+  ellipse(320,305,9,9)
+  strokeWeight(4)
+  line(318,290,330,293)
+  
+//arms
+  strokeWeight(6)
+  noFill()
+  arc(255,390,80,60,1.4,3.1)
+  arc(315,390,80,60,-.2,1.7)
 }
