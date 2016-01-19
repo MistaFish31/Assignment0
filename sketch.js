2f552fb4ecd5ad96cbdaa35de8cb24dc5310f4dc
MistaFish31 <zhang.ma@husky.neu.edu>
2016-01-19 12:53:41 -0500

    eyes follow ball
    conditional statement

--- sketch.js
+++ sketch.js
@@ -1,11 +1,20 @@
+var confettiX=15
+var confettiY=15
+
 function setup() {
   createCanvas(700,700);
 }
 
 function draw() {
+//ball
+  background(250,250,252)
+  strokeWeight(0)
+  fill(0,0,250,30)
+  ellipse(mouseX,mouseY,80,80)
 
 //legs
-  strokeWeight(6)
+  stroke(0,0,0)
+  strokeWeight(7)
   noFill()
   arc(255,520,45,180,-.6,1.6)
   arc(330,515,35,180,1.5,3.8)
@@ -14,13 +23,12 @@ function draw() {
   strokeWeight(0)
   fill(170,120,120)
   arc(360,420,95,110,-.6,2.2,CHORD)
-  fill(255,255,255)
+  fill(250,250,252)
   arc(340,395,125,125,-.6,2.2,CHORD)
 
 //Ears
   fill(170,120,120)
-  triangle(340,350,350,320,380,350)
-    triangle(340,350,350,320,380,350)
+  triangle(330,360,350,320,375,364)
   
 //body
   fill(150,100,100)
@@ -48,13 +56,20 @@ function draw() {
   strokeWeight(4)
   line(318,290,330,293)
   
-  fill(250,250,250)
-  ellipse(320,308,8,8)
+  strokeWeight(0)
+  if(mouseX<350){
+    fill(250,250,250)
+    ellipse(316,306,8,8) 
+  }else{
+    fill(250,250,250)
+    ellipse(325,308,8,8)
+  }
   
 //arms
   strokeWeight(6)
   noFill()
   arc(255,390,80,60,1.4,3.1)
-  arc(315,390,80,60,-.2,1.7);
+  arc(315,390,80,60,-.2,1.7)
+  
 
 }
